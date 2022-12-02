c771b9b8e73b35730162b6d2006893169cbe0a2a
roziisho07 <96681822+roziisho07@users.noreply.github.com>
2022-12-02 18:54:27 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,107 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import fetch from "node-fetch";
+
+const wait = (seconds = 2000) => new Promise((res) => setTimeout(res, seconds));
+const welcome = async () => {
+  await wait();
+  console.log(
+    chalk.blue("Currency Converter\n\nGet the latest exchange rates 🤑🤑\n")
+  );
+  await wait();
+  console.log(
+    chalk.yellow(
+      "How to use?\nEnter the total amount you want to convert\nEnter currency that you want to convert from,e.g: (USD, GBP, PKR, ...)\nEnter currency that you want to convert To,e.g: (USD, GBP, PKR, ...)\n"
+    )
+  );
+};
+
+const key = { apiKey: "VQK21LHhh9j32n8uhYs9ZXCbC90jGbiG" };
+
+const getUserInput = async () => {
+  const amount = await inquirer.prompt([
+    {
+      name: "amount:",
+      type: "input",
+      message: "Amount:",
+      validate: (input) => {
+        if (!Number(input)) {
+          return "invalid amount";
+        } else return true;
+      },
+    },
+  ]);
+  const from = await inquirer.prompt([
+    {
+      name: "From:",
+      type: "input",
+      message: "From:",
+      validate: (input) => {
+        if (Number(input) || input.length != 3) {
+          return "invalid currency code";
+        } else return true;
+      },
+    },
+  ]);
+  const to = await inquirer.prompt([
+    {
+      name: "To:",
+      type: "input",
+      message: "To:",
+      validate: (input) => {
+        if (Number(input) || input.length != 3) {
+          return "invalid currency code";
+        } else return true;
+      },
+    },
+  ]);
+
+  try {
+    const response = await fetch(
+      `https://api.apilayer.com/exchangerates_data/convert?to=${to["To:"]}&from=${from["From:"]}&amount=${amount["amount:"]}`,
+      {
+        method: "GET",
+        redirect: "follow",
+        headers: key,
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Error fetching data!");
+    }
+
+    const data: any = await response.json();
+    const rate: number = data.info.rate;
+    const result: number = data.result;
+    const convertedFrom: string = data.query.from;
+    const convertedTo = data.query.to;
+
+    console.log(
+      `\n${chalk.yellow("Converted")}\n${chalk.green(
+        convertedFrom
+      )} ${chalk.white("TO")} ${chalk.green(convertedTo)}
+      \nConverted Amount: ${chalk.blue(result)}\nConversion Rate: ${chalk.blue(
+        rate
+      )}\n`
+    );
+  } catch (error) {
+    console.log(chalk.red("Error! Invalid Currency code"));
+  }
+};
+
+let isValid = false;
+const continueConversion = async () => {
+  const continueProcess = await inquirer.prompt({
+    name: "continue:",
+    type: "confirm",
+    message: "Do you want to convert another currency:",
+  });
+  isValid = continueProcess["continue:"];
+};
+
+console.clear();
+await welcome();
+do {
+  console.log("");
+  await getUserInput();
+  await continueConversion();
+} while (isValid);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Currency Converter
+
+command: npx currencyconverter-rozi
